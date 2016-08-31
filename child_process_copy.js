14f608f41e079be82a3e2005e88b98ffce9cb6dd
Rey Ramos <ramor11@labcorp.com>
2016-08-31 13:27:25 -0400

    fix copy command

--- child_process_copy.js
+++ child_process_copy.js
@@ -8,7 +8,8 @@ var path = require("path");
 function copy(from, to) {
   from = path.join(angularCliConfig.apps[0].root, from.replace(/\//gim, "\\"));
   to = path.join(angularCliConfig.apps[0].outDir, to.replace(/\//gim, "\\"));
-  child.exec("xcopy /y /q \"" + from + "\\*\" \"" + to + "\\\"");
+  var command = ["xcopy /e /y /q  \"", from + "\\*\" \"", to + "\\\""].join("");
+  child.exec(command);
 }
 
 
